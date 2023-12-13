1e4040edb1ec0a146be303b6612d3da09ab8b780
professeurburp <remi.corbineau@gmail.com>
2023-12-13 10:54:05 +0100

    Step2 test with fixed lengths

--- string-calculator-kata-tests/CalculatorTests.cs
+++ string-calculator-kata-tests/CalculatorTests.cs
@@ -12,11 +12,27 @@ public class CalculatorTests
             { "1", 1 },
             { "1,2", 3 },
         };
-
+    
+    public static TheoryData<string, int> Step2Data =
+        new()
+        {
+            { "", 0 },
+            { "1", 1 },
+            { "1,2", 3 },
+            { "1,2,3", 6 },
+            { "1,2,1,3,5,6", 18 },
+        };
     
     [Theory]
     [MemberData(nameof(FirstData))]
     public void InitialTests(string input, int output)
+    {
+        new StringCalculator().Add(input).Should().Be(output);
+    }
+    
+    [Theory]
+    [MemberData(nameof(Step2Data))]
+    public void Step2Test(string input, int output)
     {
         new StringCalculator().Add(input).Should().Be(output);
     }
