e628f112e9f437327a1478b0a3ea2b7864afa80f
Sebastien ROBERT <sebastien.saigo@gmail.com>
2023-12-13 11:07:11 +0100

    Changed Add to get second test to pass

--- string-calculator-kata/Program.cs
+++ string-calculator-kata/Program.cs
@@ -2,8 +2,36 @@
 
 public class StringCalculator
 {
+    private readonly char[] separators = [ ',', '\n' ];
+
     public int Add(string numbers)
     {
-        return numbers.Length;
+        if (numbers is null)
+        {
+            throw new ArgumentNullException(nameof(numbers));
+        }
+
+        numbers = numbers.Trim();
+
+        if (numbers.Length == 0)
+        {
+            return 0;
+        }
+
+        string[] splitEntries = numbers.Split(separators, StringSplitOptions.TrimEntries);
+
+        if (splitEntries.Any(string.IsNullOrEmpty))
+        {
+            throw new ArgumentException("Empty entry not allowed.", nameof(numbers));
+        }
+
+        if (splitEntries.Any(x => int.TryParse(x, out _) == false))
+        {
+            throw new ArgumentException("Non-integer entry not allowed.", nameof(numbers));
+        }
+
+        return splitEntries
+            .Select(int.Parse)
+            .Sum();
     }
 }
